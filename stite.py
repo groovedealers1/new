db7dfafffa5cd814f862ac18dad23e00a6953dac
groovedealers1 <153697448+groovedealers1@users.noreply.github.com>
2023-12-26 17:01:37 +1100

    Add files via upload

---
+++ stite.py
@@ -0,0 +1,17 @@
+from flask import render_template, Flask, request
+
+
+app = Flask(__name__, template_folder='template')
+
+
+@app.route('/index/', methods=['POST', 'GET'])
+@app.route('/', methods=['POST', 'GET'])
+def home():
+    if request.method == 'POST':
+        return render_template('jump.html')
+    else:
+        return render_template('home_page.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
